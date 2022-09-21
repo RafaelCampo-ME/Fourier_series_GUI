de7f43bff7e88f155072db2b8988e6518facac77
Rafael <rs.campo10@uniandes.edu.co>
2022-09-20 21:22:47 -0500

    Cambio clase fourier

--- fourier.py
+++ fourier.py
@@ -3,60 +3,38 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 import scipy as sp 
 
+class Fourier:
+  def __init__(self) -> None:
+    pass
 
-class Fourier():
-    def __init__(self) -> None:
-        pass
+  def funcion_escalon(self):
+    t= np.arange(-1*np.pi, np.pi,0.01)
+    func_escalon= lambda x: x/np.abs(x)
+    s = func_escalon(t)
+    plt.plot(t,s,'r--')
+    plt.show()
 
-    def func_triangle(self):
-        """Triangle shape function"""
-        
-        t=np.arange(-1*np.pi,np.pi,0.01)
-        s=((1/np.pi)*-(t/abs(t))*(t)+1)
-        fig = plt.plot(t,s,'r--')
-        return fig
-
-
-
-    def plot_sin(self ):
-      t = np.arange(-1*np.pi,np.pi,0.01) 
-      s= np.sin(t)
-      fig = plt.plot(t,s)
-      return fig
-       
-
-    def plot_cos(self ):
-      t = np.arange(-1*np.pi,np.pi,0.01) 
-      s=  np.cos(t)
-      fig = plt.plot(t,s)
-      return fig
-      
-
-    def plot_func(self):
-      t = np.arange(-1*np.pi,np.pi,0.01) 
-      s=  (t/np.abs(t)) 
-      fig = plt.plot(t,s,'r--')
-      return fig
-
-    def plot_error(self):
-      t = np.arange(-1*np.pi,np.pi,0.01) 
-      s=  (t/np.abs(t)) * self.amplitud.get()
-      fig = plt.plot(t,s)
-      return fig
-       
-    def fourier_escalon_coeficientes(self):
-        
-      s= lambda x: (x/np.abs(x)) 
-      termino_cos = lambda x: (x*np.cos(2*x))/np.abs(x)
-      termino_sin = lambda x: (x*np.sin(2*x))/np.abs(x)
-
-      a_0 = 1/(np.pi)*( integrate.quad(s,0, np.pi)[0] + integrate.quad(s, -np.pi, 0)[0])
-      a_n = 1/(np.pi)* ((integrate.quad( termino_cos,-np.pi,0)[0])+(integrate.quad( termino_cos,0,np.pi)[0])) 
-      b_n = 1/(np.pi)* ((integrate.quad( termino_sin,-np.pi,0)[0])+(integrate.quad( termino_sin,0,np.pi)[0])) 
-
-      return (a_0,a_n,b_n)
+  def fourier_escalon(self,num_expansion):
+    t= np.arange(-1*np.pi, np.pi,0.01)
+    func_escalon= lambda x: x/(np.abs(x))
+    func_cos = lambda x,y: np.cos(2*y*x/np.pi)
+    func_sin = lambda x,y: np.sin(2*y*x/np.pi)
 
 
-      
-f = Fourier()
-print(f.fourier_escalon_coeficientes())
+
+    a_0 = []
+    for i in range(len(t)):
+      s=0
+      for j in range(num_expansion):
+        a_n = (2/np.pi)*(integrate.quad(lambda x: func_escalon(x)*func_cos(x,j),-np.pi,0)[0]+integrate.quad(lambda x: func_escalon(x)*func_cos(x,j),0,np.pi)[0])
+        b_n = (2/np.pi)*(integrate.quad(lambda x: func_escalon(x)*func_sin(x,j),-np.pi,0)[0]+integrate.quad(lambda x: func_escalon(x)*func_sin(x,j),0,np.pi)[0])
+        s=s+func_escalon(t[i])*a_n*func_cos(t[i],j) + b_n*func_escalon(t[i])*func_sin(t[i],j) 
+      a_0.append(s) 
+    s = func_escalon(t)
+
+    plt.plot(t,a_0,'r--')
+    plt.show()
+ 
+f= Fourier()
+plt.grid()
+f.fourier_escalon(100)
