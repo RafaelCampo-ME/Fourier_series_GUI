d77a17e05821cc45e099c1689f3e90e3479ef2da
Rafael <rs.campo10@uniandes.edu.co>
2022-09-20 23:16:03 -0500

    Change Fourier_escalon method

--- fourier.py
+++ fourier.py
@@ -14,23 +14,22 @@ class Fourier:
     plt.plot(t,s,'r--')
     plt.show()
 
-  def fourier_escalon(self,num_expansion):
+  def fourier_escalon(self,num_expansion:int):
     t= np.arange(-1*np.pi, np.pi,0.01)
     func_escalon= lambda x: x/(np.abs(x))
-    func_cos = lambda x,y: np.cos(2*y*x/np.pi)
-    func_sin = lambda x,y: np.sin(2*y*x/np.pi)
+    func_cos = lambda x,y: np.cos(y*x )
+    func_sin = lambda x,y: np.sin(y*x)
 
-
+     
 
     a_0 = []
     for i in range(len(t)):
       s=0
       for j in range(num_expansion):
-        a_n = (2/np.pi)*(integrate.quad(lambda x: func_escalon(x)*func_cos(x,j),-np.pi,0)[0]+integrate.quad(lambda x: func_escalon(x)*func_cos(x,j),0,np.pi)[0])
-        b_n = (2/np.pi)*(integrate.quad(lambda x: func_escalon(x)*func_sin(x,j),-np.pi,0)[0]+integrate.quad(lambda x: func_escalon(x)*func_sin(x,j),0,np.pi)[0])
-        s=s+func_escalon(t[i])*a_n*func_cos(t[i],j) + b_n*func_escalon(t[i])*func_sin(t[i],j) 
+        a_n = (1/np.pi)*(integrate.quad(lambda x: func_escalon(x)*func_cos(x,j),-np.pi,0)[0]+integrate.quad(lambda x: func_escalon(x)*func_cos(x,j),0,np.pi)[0])
+        b_n = (1/np.pi)*(integrate.quad(lambda x: func_escalon(x)*func_sin(x,j),-np.pi,0)[0]+integrate.quad(lambda x: func_escalon(x)*func_sin(x,j),0,np.pi)[0])
+        s=s+a_n*func_cos(t[i],j) + b_n*func_sin(t[i],j) 
       a_0.append(s) 
-    s = func_escalon(t)
 
     plt.plot(t,a_0,'r--')
     plt.show()
