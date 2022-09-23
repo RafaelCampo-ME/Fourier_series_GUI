2556e2797fbdf09be7d753931e11cd4caaf85d34
Rafael <rs.campo10@uniandes.edu.co>
2022-09-22 19:21:26 -0500

    Change in Fourier series class

--- fourier.py
+++ fourier.py
@@ -1,5 +1,3 @@
-from re import S
-from xmlrpc.client import Boolean
 import numpy as np 
 import matplotlib.pyplot as plt 
 from scipy import integrate
@@ -58,7 +56,7 @@ class Fourier:
 		return (t, a_0)
 	
 	
-	def fourier_series_aprox(self,num_expansion:int,**func_name:str):
+	def fourier_series_aprox(self,num_expansion:int,func_name:str):
 		"""Return a tuple object of the Fourier serie aproximation of function"""
 		
 		func_lambda = lambda x: x/np.abs(x)
@@ -66,7 +64,7 @@ class Fourier:
 		if func_name == 'Square':
 			func_lambda = lambda x: x/np.abs(x)
 		elif func_name == 'Triangle':
-			func_lambda = lambda x: ((1/np.pi)*-(x/abs(x))*(x)+1)
+			func_lambda = lambda x: ((1/np.pi)*-(x/abs(x))*(x)+1)   
 		elif func_name == 'Random_sin_cos':
 			func_lambda = lambda x: np.abs(np.sin(x)) + np.cos(x)
 		else:
@@ -75,23 +73,39 @@ class Fourier:
 		t= np.arange(-1*np.pi, np.pi,0.01)   ## t represents the time (x axis)
 		func_cos = lambda x,y: np.cos(y*x)
 		func_sin = lambda x,y: np.sin(y*x)
+		a_0 =  (1/np.pi) * (integrate.quad(func_lambda,-1*np.pi,0)[0]+integrate.quad(func_lambda,0,np.pi)[0])
 
-		a_0 = []
+		s_list = []
 		for i in range(len(t)):
-			s=0
+			s= a_0
 			for j in range(num_expansion):
+				
 				a_n = (1/np.pi)*(integrate.quad(lambda x: func_lambda(x)*func_cos(x,j),-np.pi,0)[0]+integrate.quad(lambda x: func_lambda(x)*func_cos(x,j),0,np.pi)[0])
 				b_n = (1/np.pi)*(integrate.quad(lambda x: func_lambda(x)*func_sin(x,j),-np.pi,0)[0]+integrate.quad(lambda x: func_lambda(x)*func_sin(x,j),0,np.pi)[0])
 				s=s+a_n*func_cos(t[i],j) + b_n*func_sin(t[i],j) 
-			a_0.append(s) 
+			s_list.append(s) 
 
-		return (t, a_0)
+		return (t, s_list)
 
 
-	def error_function(self,num_expan:int,abs_error:Boolean):
+	def error_function(self,num_expan:int = 1,func_name:str='Square',abs_error:bool = True):
 		"""Return a tuple object of the % of error between a square pulse function and his Fourier series aproximation, where 0 element is the time series and the 1 element is the y series"""
 		escalon = self.function_square_pulse()
-		aprox_fourier = self.fourier_escalon(num_expansion=num_expan)
+		aprox_fourier = self.fourier_series_aprox(num_expansion=num_expan,func_name='Square')
+
+		if func_name == 'Square':
+			escalon = self.function_square_pulse()
+			aprox_fourier = self.fourier_series_aprox(num_expansion=num_expan,func_name='Square')
+		elif func_name == 'Triangle':
+			escalon = self.function_triangle_pulse()  
+			aprox_fourier = self.fourier_escalon(num_expansion=num_expan,func_name='Triangle')
+		elif func_name == 'Random_sin_cos':
+			escalon = self.function_cos_sin_random_pulse() 
+			aprox_fourier = self.fourier_escalon(num_expansion=num_expan,func_name='Random_sin_cos')
+	
+		
+
+		#aprox_fourier = self.fourier_escalon(num_expansion=num_expan)
 		error= []
 
 		if abs_error ==True:
@@ -103,7 +117,7 @@ class Fourier:
 				er=np.abs((aprox_fourier[1][i]-escalon[1][i]))/escalon[1][i]
 				error.append(er)
 
-		print(f"Tamaño de la listta error: {len(error)}")
+		print(f"Tamaño de la lista error: {len(error)}")
 		print(f"Tamaño de la listta error: {len(escalon[0])}")
 		return(escalon[0],error)
 
