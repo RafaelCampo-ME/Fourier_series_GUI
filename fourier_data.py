971af3dff93b920d98047fc44867f2697fdd4d32
Rafael <rs.campo10@uniandes.edu.co>
2022-09-27 22:42:07 -0500

    Fourier series class (new): Dummie methods implemeted

--- fourier_data.py
+++ fourier_data.py
@@ -1,12 +1,12 @@
 import numpy as np
 import logging
-
+from scipy import integrate
 
 
 class fourierSeries:
 
 
-	def __init__(self,function_name,num_expansion = 1, function_directory) -> None:
+	def __init__(self,function_name:str, num_expansion:int = 1, function_directory:dict={}) -> None:
 		self.function_name = function_name
 		self.num_expansion = num_expansion
 		self.function_directory = function_directory 
@@ -19,9 +19,21 @@ class fourierSeries:
 	def fourier_series_aprox(self)->list:
 		logging.info("Fourier series has been calculated succesfully")
 		return (list)
-
 	
-	def fourier_series_info(self):
+
+	def fast_fourier_series()->list:
+		return ()
+
+
+	def error_function()->list:
+		return ()
+
+
+	def average_error()->float:
+		return()
+
+
+	def fourier_series_info(self,aprox_type:str = 'LFS'):
 
 			if self.function_name not in self.function_directory.keys():
 				raise ValueError (f"{self.function_name} had not been implemented yet")
@@ -29,26 +41,16 @@ class fourierSeries:
 			if self.num_expansion < 0:
 				raise ValueError (f"Number of Fourier expansion series must be greater that 0. Number used:{self.num_expansion}")
 			
-			self.fourier_series_aprox()
+			if aprox_type not in ('LFS','FFT'):
+				raise ValueError (f"{aprox_type} is a unsupported aproximation type.")
+
+			t = self.time_series()
+
+			if aprox_type == 'LFS':
+				s = self.fourier_series_aprox()
+			else:
+				s = self.fast_fourier_series()
 			
-
-
-		 
-	 
-
-		
-
-
-f = fourierSeries("Square",1)
-f.fourier_series_info()
-
-
-
-
-
-
-
-
-
-
+			return t,s
+			
 
