932e9090e7361be95977345ce2910f6e336fea59
Rafael <rs.campo10@uniandes.edu.co>
2022-10-10 15:52:09 -0500

    javascript

---
+++ plot.py
@@ -0,0 +1,17 @@
+from fourier_data import fourierSeries 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+func_directory = { 'Square':   lambda x: x/np.abs(x)}
+expansion = 49
+
+f = fourierSeries(function_name='Square',function_directory=func_directory,num_expansion=expansion)
+f = f.fourier_series_info()
+plt.subplot()
+plt.plot(f[0],f[1])
+plt.plot(f[0],f[2])
+plt.plot(f[0],f[3])
+print(f[4])
+plt.show()
+#print(f)
