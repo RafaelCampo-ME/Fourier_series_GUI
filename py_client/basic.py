a540ab0e496349482325b328a9d4b7141ad06d60
Rafael <rs.campo10@uniandes.edu.co>
2022-11-02 18:46:03 -0500

    Commit in basic.py

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,14 +1,22 @@
 import re
 from urllib import request
-
-
 import requests  
 
-endpoint = "https://httpbin.org/anything"
- 
+endpoint = "http://localhost:8000/" ##This is my localhost with the 8000 port 
 
 get_response = requests.get(endpoint) #Metodo para traer API
 
-print(f"Respuesta del servicio: {get_response}" ) 
-print(f"Nuestra respuesta JSON \n{get_response.text)}"  
+print(f"Respuesta del servicio: {get_response}" )  
+print(f"Nuestra respuesta JSON \n{get_response.text}" ) 
 
+
+
+#def api_home(request, *args, **kwargs):
+#    body = request.body
+#    data = {}
+#    try:
+#        data = json.loads(body)
+#    except:
+#        pass
+#    print(data)
+#    data['headers'] = request.headers
