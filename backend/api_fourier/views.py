c7a12a8d238d17691450eb9b9f50138365b0fb5f
Rafael <rs.campo10@uniandes.edu.co>
2022-11-04 21:30:32 -0500

    Commit docu-api

--- backend/api_fourier/views.py
+++ backend/api_fourier/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+##from rest_framework import generics
+#from api_fourier.models import documentation
+##from  import BookSerializer
+
+
+
 # Create your views here.
+##class BookAPIView(generics.ListAPIView):
+##    queryset = Book.objects.all()
+##    serializer_class = BookSerializer
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #local
-    
+
     'api_fourier'
 ]
 
