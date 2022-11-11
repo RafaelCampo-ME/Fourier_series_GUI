81f9310af524bcd831d4942469df94136a7157b4
Rafael <rs.campo10@uniandes.edu.co>
2022-11-10 22:02:28 -0500

    Modicicacion en metodo para reproducir audio

---
+++ backend/api_fourier/FourierLogic/audio_test.py
@@ -0,0 +1,38 @@
+from scipy.io.wavfile import read
+from os.path import dirname, join as pjoin
+import scipy.io
+
+#data_dir = pjoin(dirname(scipy.io.__file__),'tests', 'data')
+#wav_fname = pjoin(data_dir,'test-44100Hz-2ch-32bit-float-be.wav')
+
+#data_dir = pjoin(dirname('/FourierLogic'), 'audio_test_files')
+#wav_fname = pjoin(data_dir,'Jairo and Diego on the rocks.wav')
+
+
+samplerate, data = read(r'Fourier_series_GUI\backend\api_fourier\FourierLogic\audio_test_files\Jairo and Diego on the rocks.wav')
+print(f"number of channels = {data.shape[1]}")
+
+length = data.shape[0] / samplerate
+print(f"length = {length}s")
+
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+
+stime = np.linspace(0., length, data.shape[0])
+plt.plot(stime, data[:, 0], label="Left channel")
+plt.plot(stime, data[:, 1], label="Right channel")
+plt.legend()
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude")
+plt.show()
+
+import sounddevice as sd
+sps = 48000 
+#Este son los hz adec
+
+sd.play(data[:, 0],sps)
+time.sleep(10)
+sd.stop()
+print("final")
