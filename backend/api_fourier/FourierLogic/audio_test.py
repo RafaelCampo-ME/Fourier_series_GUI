db1e2fc12f04956e20506d8420490381370e6b41
Rafael <rs.campo10@uniandes.edu.co>
2022-11-16 06:32:56 -0500

    LOW PASS

--- backend/api_fourier/FourierLogic/audio_test.py
+++ backend/api_fourier/FourierLogic/audio_test.py
@@ -30,3 +30,21 @@ sd.play(data[:, 0],sps)
 time.sleep(10)
 sd.stop()
 print("final")
+
+
+
+#### This section will cover the lowpass filters
+
+from scipy.signal import butter, lfilter, freqz
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
